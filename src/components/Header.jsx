6e284edbcc9bb9bf98cdf10f24dc55a9dd23f1df
Сергей Маркушин <kramgres@yandex.ru>
2021-01-20 23:20:10 +0700

    test commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://logos-download.com/wp-content/uploads/2016/07/Telegram_logo_icon.png'></img>
+        </header>
+    )
+}
+
+export default Header;
